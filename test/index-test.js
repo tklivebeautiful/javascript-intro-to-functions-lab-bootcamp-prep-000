1fc3b1445e95955d99a1532f3921c40bc4a09cb7
Tara Kaestner <tara.livebeautiful@gmail.com>
2017-09-21 01:41:16 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -55,15 +55,15 @@ function logShout(string) { console.log(string.toUpperCase()) }
 
 function logShout(string) {console.log(string.toUpperCase()) }
 function logWhisper(string) {console.log(string.toLowerCase())}
-function sayHiToGrandma (string) {
+function sayHiToGrandma (string) 
   if (string.toLowerCase() ===
-string) {
-  return "I can't hear you!"
+  string) {
+  return "I can't hear you!" 
+} else if (string.toUpperCase() ===
+  string) {
+  return "YES INDEED!"
+} else {
+  return "I love you, too."
 }
 }
 
-function sayHiToGrandma (string)
-  {if (string.toUpperCase() ===
-  string)
-  {return "YES INDEED!"}
-}
